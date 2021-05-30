3bc01bfb0b64fa951d276bb44890fa14326d4c57
rajakushwah <kushwah.raja1@gmail.com>
2021-05-30 21:47:10 +0530

    static

--- PatientBrowser/settings.py
+++ PatientBrowser/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #add your static files in this directory
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/app/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
