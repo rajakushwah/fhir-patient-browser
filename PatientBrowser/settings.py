77905310f42ba101bb558d4f027b81a39ac3dd3f
rajakushwah <kushwah.raja1@gmail.com>
2021-06-03 19:10:02 +0530

    Setting

--- PatientBrowser/settings.py
+++ PatientBrowser/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4xp-l%0#v-m-+5c$@a@3kn@pj((1(s%9zqnrx4%2$-qe)18sg)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
